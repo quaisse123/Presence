db6ef748853e8501942bcf75bf693602fd24add8
quaisse123 <marouanequaisse@gmail.com>
2026-09-08 00:11:15 +0200

    Nettoyer les imports dans JwtService

--- frontend/lib/Api/JwtService.dart
+++ frontend/lib/Api/JwtService.dart
@@ -1,9 +1,8 @@
 import 'dart:convert';
 
-// import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:frontend/config/apiUrl.dart';
 import 'package:frontend/pages/login.dart';
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
